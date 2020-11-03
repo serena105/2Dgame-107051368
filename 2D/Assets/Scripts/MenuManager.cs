62d9dfd63cea8583c5772d6a876c431fa38917dc
serena105 <71679914+serena105@users.noreply.github.com>
2020-11-03 16:09:01 +0800

    碰撞偵測

--- 2D/Assets/Scripts/MenuManager.cs
+++ 2D/Assets/Scripts/MenuManager.cs
@@ -1,16 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
- void StartGame()
-    {
+ 
+    // private 私人  public 公開(可被按鈕呼叫)
+   //修飾詞 類型 方式名稱( ) { }
+   //void 無類型
+ public void StartGame()
+ {
+  print("開始遊戲");
+  SceneManager.LoadScene("關卡一");
+ }
 
-    }
 
- void QuitGame()
-    {
+ public void QuitGame()
+ {
+  print("結束遊戲");
+  Application.Quit();
 
-    }
+ }
 
 
   
---
+++ 2D/Assets/Scripts/player.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    // 空物值 (None)導致程式錯誤-閃退...等
+
+    public GameObject exit;
+
+    //碰撞偵測條件
+    //1.兩個物件必須有碰撞器 (Collider2D)
+    //2.兩者必須有至少一個剛體 (Rigidbody)
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("碰到傳送門");
+        exit.SetActive(true);
+    }
+}
